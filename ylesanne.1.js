c9ec04e5c156c988d627bb37e690b11f6f1b55c4
fredy <fredy-andri.mottus@voco.ee>
2024-12-04 13:34:14 +0200

    ülesanne1

---
+++ ylesanne.1.js
@@ -0,0 +1,15 @@
+class Student {
+    constructor(name, finished ){
+        this.name = 'John';
+        this.finished = false;
+    } 
+}  
+const student1 = new Student()
+student1.name = 'John'
+student1.finished = false
+console.log(student1)
+
+const student2 = new Student()
+student2.name = 'Mary'
+student2.finished = false
+console.log(student2)
